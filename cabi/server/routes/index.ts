0e7542a7540afa2c939e56e6022f9f8481de6f7f
subin195-09 <k_subin@naver.com>
2021-12-19 16:22:06 +0900

    12/19 cabi 개발환경 setting

--- cabi/server/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import * as express from 'express';
-
-const router = express.Router();
-
-router.get('/', async (req:express.Request, res:express.Response, next:express.NextFunction) => {
-	res.send({greeting: "Hello React x Node.js"});
-});
-
-export default router;
--- cabi_node/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import * as express from 'express';
-
-const router = express.Router();
-
-router.get('/', async (req:express.Request, res:express.Response, next:express.NextFunction) => {
-	res.render('index');
-});
-
-export default router;
