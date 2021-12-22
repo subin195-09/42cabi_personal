6decec29214e4141eeb6c44a24fc951efc7e7f52
subin195-09 <k_subin@naver.com>
2021-12-22 18:16:27 +0900

    12/22 swagger setting

--- 42cabi_back/src/app.ts
+++ 42cabi_back/src/app.ts
@@ -1,4 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
+import path from 'path';
+import swaggerUI from 'swagger-ui-express'
+import YAML from 'yamljs'
 
 const app = express();
 
@@ -6,10 +9,13 @@ app.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.send('welcome!');
 });
 
+const swaggerSpec = YAML.load(path.join(__dirname, './swagger.yaml'));
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+
 app.listen('8080', () => {
     console.log(`
   ################################################
-  🛡️  Server listening on port: 1234🛡️
+  🛡️  Server listening on port: 8080
   ################################################
 `);
 });
