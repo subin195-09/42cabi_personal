da7961bae9d2293113439246b85b5ff4298e2137
subin195-09 <k_subin@naver.com>
2021-12-28 02:23:07 +0900

    12/28 nodejs + mysql 연동하기

--- 42cabi_back/src/app.ts
+++ 42cabi_back/src/app.ts
@@ -4,17 +4,38 @@ import swaggerUI from 'swagger-ui-express'
 import YAML from 'yamljs'
 
 function makeServer(){
-    const app = express();
-    const port = 4242;
+	const app = express();
+	const port = 4242;
 
-    const swaggerSpec = YAML.load(path.join(__dirname, './swagger.yaml'));
-    app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
-    app.listen(port, ()=>console.log(`Listening on port ${port}`));
+	const mysql = require('mysql');
+	const con = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '0909',
+	database: 'skim_cabi'
+	});
 
-    app.use(express.static(path.join(__dirname, '../public')));
-    app.use('/', function(req, res){
-        res.sendFile(path.join(__dirname, '../public/index.html'));
-    });
+	app.use('/db', function(req, res){
+		con.connect(function(err: any) {
+			if (err) throw err;
+			console.log('connected');
+		});
+
+		con.query('SELECT * FROM user', function (err: any, result: any, fields: any) {
+			if (err) throw err;
+			console.log(result);
+			res.send(result);
+		});
+	});
+
+	const swaggerSpec = YAML.load(path.join(__dirname, './swagger.yaml'));
+	app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+	app.listen(port, ()=>console.log(`Listening on port ${port}`));
+
+	app.use(express.static(path.join(__dirname, '../public')));
+	app.use('/', function(req, res){
+		res.sendFile(path.join(__dirname, '../public/index.html'));
+	});
 }
 
 makeServer();
